f1b08e9c83f92aec559b6c87cf9d02990f04e284
nabinyang <nabinyang73@gmail.com>
2023-04-20 09:23:24 +0900

    upload test

--- test.dart
+++ test.dart
@@ -1,7 +1,7 @@
 class Food {
   final String foodType;
 
-  Food(this.foodType);
+  Food({required this.foodType});
 
   void sayFoodType() {
     print('This food type is $foodType.');
@@ -12,7 +12,7 @@ class Snack extends Food {
   final String name;
   final String brand;
 
-  Snack(super.foodType, this.name, this.brand);
+  Snack({required super.foodType, required this.name, required this.brand});
 
   void sayIntroduce() {
     print('This is $brand\'s $name.');
@@ -20,7 +20,7 @@ class Snack extends Food {
 }
 
 void main() {
-  Snack ccobook = Snack('snack', '꼬북칩', '오리온');
+  Snack ccobook = Snack(foodType: 'snack', name: '꼬북칩', brand: '오리온');
 
   ccobook.sayIntroduce();
   ccobook.sayFoodType();
